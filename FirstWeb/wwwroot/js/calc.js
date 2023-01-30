60ae0e8db69202289e0ea4217ff5d5706210c428
papichulo <papichulo@EDUARDORODR66E3>
2023-01-30 15:20:35 -0700

    Add project files.

---
+++ FirstWeb/wwwroot/js/calc.js
@@ -0,0 +1,60 @@
+﻿$('#calc').click(function (){
+   /* getting all the values from the DOM*/
+    let ass = parseFloat($("#Ass").val());
+    let gro = parseFloat($("#Gro").val());
+    let quiz = parseFloat($("#Quiz").val());
+    let mid = parseFloat($("#Midterm").val());
+    let final = parseFloat($("#Final").val());
+    let intex = parseFloat($("#Intex").val());
+  /*  calcuating the weight of the grades*/
+    let total = (ass * 0.5) + (gro * 0.1) + (quiz * 0.1) + (mid * 0.1) + 
+        (final * 0.1) + (intex * 0.1);
+    let score = total.toFixed(2);
+    let grade = "";
+
+    /*calcuating the letter grade*/
+
+    if (score >= 94) {
+        grade = "A";
+    }
+    else if (score >= 90) {
+        grade = "A-";
+    }
+    else if (score >= 87) {
+        grade = "B+";
+    }
+    else if (score >= 84) {
+        grade = "B";
+    }
+    else if (score >= 80) {
+        grade = "B-";
+    }
+    else if (score >= 77) {
+        grade = "C+";
+    }
+    else if (score >= 74) {
+        grade = "C";
+    }
+    else if (score >= 70) {
+        grade = "C-";
+    }
+    else if (score >= 67) {
+        grade = "D+";
+    }
+    else if (score >= 64) {
+        grade = "D";
+    }
+    else if (score >= 60) {
+        grade = "D-";
+    }
+    else if (score <= 60) {
+        grade = "E";
+    }
+
+    $("#show").html("You wil get a "+ score+"% " + "and a letter grade of: "+grade);
+
+    console.log(grade, score);
+    
+
+
+})
