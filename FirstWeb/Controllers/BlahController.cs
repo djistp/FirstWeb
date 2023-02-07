1949ef329cc3c7baec242a277c2f39d946c27fb7
Eduardo Rodriguez <eduardoarodriguez272@gmail.com>
2023-02-07 03:38:18 -0700

    did this work

---
+++ FirstWeb/Controllers/BlahController.cs
@@ -0,0 +1,28 @@
+﻿using FirstWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWeb.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Calc()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calc (CalcModel model)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ FirstWeb/Models/CalcModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWeb.Models
+{
+    public class CalcModel
+    {
+        //grabbing all the inputs and making sure its from the speficied range
+        [Required]
+        [Range(0,100)]
+        public float Assigments { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float Gro { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float Quiz { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float Midterm { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float Final { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float Intex { get; set; }
+
+    }
+}
